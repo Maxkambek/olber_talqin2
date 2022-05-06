94e2680bb89a95b56a395206b4e8e5a689599e96
ShuhratX <shuhratxayitov@gmail.com>
2022-05-06 10:57:02 +0500

    verify email

--- user/views.py
+++ user/views.py
@@ -13,31 +13,6 @@ from .serializers import LoginSerializer, UserSerializer, CargoSerializer, Cargo
     RegisterSerializer, VerifySerializer
 
 
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-    permission_classes = (AllowAny,)
-
-    @staticmethod
-    def post(request):
-        try:
-            email = request.data.get('email')
-            password = request.data.get('password')
-            user = User.objects.get(email=email)
-            check = user.check_password(password)
-            verify = user.is_verified
-            print(verify)
-            if check and verify:
-                token, created = Token.objects.get_or_create(user=user)
-                return Response({
-                    'token': token.key,
-                    'email': email
-                })
-            else:
-                return Response("Неверное имя пользователя или пароль", status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
@@ -52,14 +27,18 @@ class RegisterView(generics.GenericAPIView):
             code = str(random.randint(100000, 1000000))
             msg = "Emailni tasdiqlash uchun bir martalik kod "
             to = request.data.get('email')
-            res = send_mail(subject, str(msg), settings.EMAIL_HOST_USER, [to])
-            if (res == 1):
+            result = send_mail(subject, str(msg), settings.EMAIL_HOST_USER, [to])
+            verify = VerifyEmail.objects.get(email=email)
+            if verify:
+                verify.delete()
+            if (result == 1):
                 msg1 = str(msg) + to + " ga jo'natildi "
                 VerifyEmail.objects.create(email=email, code=code)
                 User.objects.create_user(email=email, username=username, password=password)
+                print(code)
             else:
-                msg1 = "Ma'lumotlarda xatolik!"
-            print(email)
+                return Response({"msg" : "Ma'lumotlarda xatolik bor yoki verifikatsiya uchun kod emailingizga jo'natilgan!"},
+                                status=status.HTTP_400_BAD_REQUEST)
             return Response({
                 "email": email,
                 "username": username,
@@ -74,16 +53,43 @@ class VerifyView(generics.GenericAPIView):
     def post(self, request):
         email = request.data.get('email')
         code = request.data.get('code')
-        if VerifyEmail.objects.filter(email=email, code=code).first():
+        verify = VerifyEmail.objects.filter(email=email, code=code).first()
+        if verify:
             user = User.objects.get(email=email)
             user.is_verified = True
             user.save()
+            verify.delete()
             return Response({
                     'msg': "Email is verified",
                     'email': email
                 }, status=status.HTTP_200_OK)
         else:
             return Response("Email or code invalid", status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = (AllowAny,)
+
+    @staticmethod
+    def post(request):
+        try:
+            email = request.data.get('email')
+            password = request.data.get('password')
+            user = User.objects.get(email=email)
+            check = user.check_password(password)
+            verify = user.is_verified
+            if check and verify:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                    'token': token.key,
+                    'email': email
+                })
+            else:
+                return Response("Неверное имя пользователя или пароль", status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 class UsersView(generics.ListAPIView):
     serializer_class = UserSerializer
