6a9d696f4238f800b5f76ad637b565b66f8b4b26
ShuhratX <shuhratxayitov@gmail.com>
2022-06-06 09:51:35 +0500

    cargo offer

--- user/serializers.py
+++ user/serializers.py
@@ -135,7 +135,7 @@ class UserDetailSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("email", 'username', 'first_name', 'last_name', 'phone', 'works', 'telegram', 'gender', 'rating')
+        fields = ("email", 'username', 'first_name', 'last_name', 'phone', 'works', 'telegram', 'gender', 'image', 'rating')
 
 
 class TestSerializer(serializers.ModelSerializer):
