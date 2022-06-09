da3d0dafa5eaada1005ba3f55a2c50f557b93cfd
ShuhratX <shuhratxayitov@gmail.com>
2022-06-09 15:03:20 +0500

    datefield

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
+    "DATE_INPUT_FORMATS": ["%d-%m-%Y"],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
