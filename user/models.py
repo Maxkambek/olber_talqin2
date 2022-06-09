158d18bb084268420c210dff61bb9a303dba4a82
ShuhratX <shuhratxayitov@gmail.com>
2022-06-09 16:03:36 +0500

    datefield

--- user/models.py
+++ user/models.py
@@ -59,7 +59,7 @@ class Cargo(models.Model):
     to_floor = models.PositiveIntegerField()
     to_kv = models.PositiveIntegerField()
     to_persons = models.PositiveIntegerField()
-    time_when = models.DateField()
+    time_when = models.CharField(max_length=35)
     description = models.TextField(verbose_name="Tafsilot")
     image1 = models.ImageField()
     image2 = models.ImageField(null=True, blank=True)
