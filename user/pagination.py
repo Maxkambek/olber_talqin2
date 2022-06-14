538db7ae03fa4a84f122fbdf28c2687b24ffde05
ShuhratX <shuhratxayitov@gmail.com>
2022-06-14 10:31:40 +0500

    pagination

--- user/pagination.py
+++ user/pagination.py
@@ -2,6 +2,6 @@ from rest_framework import pagination
 
 
 class CustomPagination(pagination.CursorPagination):
-    page_size = 10
+    page_size = 5
     cursor_query_param = 'c'
     ordering = '-created'
