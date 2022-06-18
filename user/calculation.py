c05a6915feb7e73dbae64825f8c7472ad628f067
ShuhratX <shuhratxayitov@gmail.com>
2022-06-18 16:54:47 +0500

    debuging

--- user/calculation.py
+++ user/calculation.py
@@ -20,14 +20,3 @@ def calc_distance(address1, address2):
 
     meters = R * c  # output distance in meters
     return meters / 1000.0  # output distance in kilometers
-
-
-from_add = "41.337217 69.284756"
-to_add = "41.326756 69.282663"
-
-lat1 = float(from_add.split()[0])
-lon1 = float(from_add.split()[1])
-lat2 = float(to_add.split()[0])
-lon2 = float(to_add.split()[1])
-# print(calc_distance(lat1, lon1, lat2, lon2))
-# print(lat1, lon1, lat2, lon2)
