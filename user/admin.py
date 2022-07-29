8518f4b79be0b404767d5f97544398cc543e314c
ShuhratX <shuhratxayitov@gmail.com>
2022-07-29 10:01:27 +0500

    ignore staticfiles

--- user/admin.py
+++ user/admin.py
@@ -8,6 +8,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email')
     readonly_fields = ('rating',)
 
+
 class CargoAdmin(TranslationAdmin):
     list_display = ('title', 'price', 'status', 'cargo_type', 'description', 'distance')
     readonly_fields = ('from_address', 'to_address', 'distance', 'doer')
