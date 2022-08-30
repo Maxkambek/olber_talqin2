a0a88bc8855e2b7dad083977fcd6868d344dfc30
ShuhratX <shuhratxayitov@gmail.com>
2022-08-30 14:37:23 +0500

    verify check

--- user/models.py
+++ user/models.py
@@ -40,6 +40,7 @@ class User(AbstractUser):
     drive_doc = models.ImageField(verbose_name="Guvohnoma", null=True, blank=True)
     car_image_1 = models.ImageField(verbose_name="Mashina rasmi", null=True, blank=True)
     car_image_2 = models.ImageField(verbose_name="Mashina rasmi", null=True, blank=True)
+    car_number = models.CharField(max_length=8, verbose_name="Davlat raqami")
     account = models.CharField(max_length=50, unique=True)
     money = models.PositiveIntegerField(default=0)
 
