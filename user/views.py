9eb8ca618bbffbc966d788470fac6b820fc36b0e
ShuhratX <shuhratxayitov@gmail.com>
2022-09-06 10:03:44 +0500

    payme merchant API debug

--- user/views.py
+++ user/views.py
@@ -658,8 +658,14 @@ class CartCreate(generics.GenericAPIView):
         number = request.data.get('card')
         expire = request.data.get('expire')
         result = payme_subscribe_cards._cards_create(123, number, expire, True)
-        print(result)
-        return Response(result)
+        if result['error']:
+            return Response({
+                "msg": "Неверные данные"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({
+                "msg": "Регистрация прошла успешно"
+            }, status=status.HTTP_200_OK)
 
 class CartGetVerify(generics.GenericAPIView):
     serializer_class = UserSerializer
