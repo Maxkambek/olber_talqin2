57fb25dd973741fd6dc4fa836a3237b7e6b24804
ShuhratX <shuhratxayitov@gmail.com>
2022-09-23 10:43:06 +0500

    deploy to git

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
 from django.utils.translation import gettext_lazy as _
 
 admin.site.index_title = 'OlBer'
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=[permissions.AllowAny]
+   permission_classes=(permissions.AllowAny,)
 )
 
 urlpatterns = i18n_patterns(
