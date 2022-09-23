bfda95a779e1be6ce6737d4c19c7706ea7fa51dd
ShuhratX <shuhratxayitov@gmail.com>
2022-09-23 10:44:54 +0500

    deploy to git

--- config/urls.py
+++ config/urls.py
@@ -19,35 +19,17 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework import permissions
-from drf_yasg2.views import get_schema_view
-from drf_yasg2 import openapi
 from django.utils.translation import gettext_lazy as _
 
 admin.site.index_title = 'OlBer'
 admin.site.site_header = _('OlBer Administration')
 admin.site.site_title = 'OlBer'
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,)
-)
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     # path('payment/', include('paycomuz.urls')),
-    re_path(r'^docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     prefix_default_language=False
 
 )
