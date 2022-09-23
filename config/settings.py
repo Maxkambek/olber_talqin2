274aaf84095d4f7d6c2e7e5769cfdfac7ad31b09
ShuhratX <shuhratxayitov@gmail.com>
2022-09-23 10:33:30 +0500

    deploy to git

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-    'drf_yasg',
+    'drf-yasg2',
     'django.contrib.sites',
 
     #third party packages
